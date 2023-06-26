a7875c07e7978210688e75c383f788048ee4db55
Nat <nataliealcaza11@gmail/com>
2023-06-26 10:30:35 -0700

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Natalie on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
